e39762e52258daa10c549281669eea5ee19acfcc
MatV5 <mattikryszek@gmail.com>
2022-11-08 08:56:10 +0100

    pierwsza proba

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,12 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    unsigned int licznik = 0;
+    while (licznik < towar)
+    {
+        Stocznia stocznia{};
+        Statek* s1 = stocznia();
+        licznik += (*s1).transportuj();
+    }
     return 0;
 }
